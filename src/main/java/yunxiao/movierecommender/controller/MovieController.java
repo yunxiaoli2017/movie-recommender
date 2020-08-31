f9f81b373065007095d6ece887428d5dabe75698
Yunxiao Li <yunxiaoli2017@gmail.com>
2020-08-30 17:59:40 -0700

    change from returning templates to return objects as responses to Angular frontend

--- src/main/java/yunxiao/movierecommender/controller/MovieController.java
+++ src/main/java/yunxiao/movierecommender/controller/MovieController.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import yunxiao.movierecommender.dto.RatingCreationDto;
@@ -17,6 +17,7 @@ import yunxiao.movierecommender.model.Rating;
 import yunxiao.movierecommender.service.MovieService;
 import yunxiao.movierecommender.service.RatingService;
 
+@CrossOrigin(maxAge = 3600)
 @Controller
 public class MovieController {
   
@@ -37,38 +38,19 @@ public class MovieController {
   }
   
   @GetMapping("/rate")
-  public String rateRandomMovies(@RequestParam(defaultValue = "32") int numMovies, Model model) {
+  public List<Movie> rateRandomMovies(@RequestParam(defaultValue = "32") int numMovies) {
     List<Movie> randomMovies = movieService.getRandomPopularMovies(numMovies);
-    RatingCreationDto ratingCreationDto = new RatingCreationDto();
-    ratingCreationDto.addEmptyRatings(randomMovies.size());
-    List<Rating> ratings = ratingCreationDto.getRatings();
-    for (int i = 0; i < randomMovies.size(); i++) {
-      ratings.get(i).setMovieId(randomMovies.get(i).getId());
-    }
-    model.addAttribute("form", ratingCreationDto);
-    model.addAttribute("movies", randomMovies);
-    return "rate";
+    return randomMovies;
   }
   
   @PostMapping("/recommendation")
-  public String getSubmittedRatings(@ModelAttribute RatingCreationDto form, Model model) {
+  public List<Movie> getRecommendedMoviesFromSubmittedRatings(@RequestBody RatingCreationDto form) {
     List<Rating> ratings = form.getRatings();
     ratings = ratingService.filterZeroRating(ratings);
     ratingService.decouplingNormalization(ratings);
-    if (ratings.size() < 1) {
-      model.addAttribute("error", "At least one rating has to be submitted.");
-      return "rate";
-    }
     List<Integer> recommendedMovieIds = ratingService.getRecommendedMovieIds(ratings);
     List<Movie> recommendedMovies = movieService.getRecommendedMovies(recommendedMovieIds);
-    model.addAttribute("ratings", ratings);
-    model.addAttribute("movies", recommendedMovies);
-    return "recommendation";
+    return recommendedMovies;
   }
-  
-  
-  
-  
-  
   
 }
